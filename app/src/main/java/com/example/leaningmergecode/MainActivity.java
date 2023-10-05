1a84f5c4c3f515d66f53092aeb8766b61f26ddef
Vun Sophann <vunsophann7@gmail.com>
2023-10-06 03:34:25 +0700

    bk new commit

--- app/src/main/java/com/example/leaningmergecode/MainActivity.java
+++ app/src/main/java/com/example/leaningmergecode/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.leaningmergecode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView.setText("hi");
     }
 }
