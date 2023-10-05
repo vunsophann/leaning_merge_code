258e40df7a30fd599f7cdc7f0dbe10e40cc7076f
Vun Sophann <vunsophann7@gmail.com>
2023-10-06 03:32:23 +0700

    profilt new commit

--- app/src/main/java/com/example/leaningmergecode/MainActivity.java
+++ app/src/main/java/com/example/leaningmergecode/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
